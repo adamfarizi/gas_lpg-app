65ed01cfd74d50cc9153e78a0a200886b331e3f1
Adam Farizi <adamfarizi4@gmail.com>
2023-09-11 19:50:41 +0700

    update

--- database/migrations/2023_09_11_124758_create_tb_user_table.php
+++ database/migrations/2023_09_11_124758_create_tb_user_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('tb_user', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->enum('role',['admin','agen','kurir'])->default('agen');
+            $table->string('password');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('tb_user');
     }
 };
