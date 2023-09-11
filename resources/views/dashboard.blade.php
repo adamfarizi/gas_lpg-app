691b84a6492e9407cd90ce13a36e0307352c95d6
vandkurnia <vandkurnia@gmail.com>
2023-09-11 15:22:31 +0700

    1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -96,9 +96,6 @@
             <span class="nav-link-text ms-1">Billing</span>
           </a>
         </li>
-        <li class="nav-item mt-3">
-          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
-        </li>
         <li class="nav-item">
           <a class="nav-link  " href="{{ asset('pages/profile.html') }}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
