be69d102f5b9a4a40269b75394b62651ef4539ef
Adam Farizi <adamfarizi4@gmail.com>
2023-09-11 13:38:51 +0700

    update multi user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,7 +81,7 @@ class UserController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('login');
+        return redirect('home');
     }
 
 }
