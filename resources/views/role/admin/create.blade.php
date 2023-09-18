c51078e7bdee4a86c60592d1a66f1cee547f9d98
Adam Farizi <adamfarizi4@gmail.com>
2023-09-18 10:44:59 +0700

    update user

--- resources/views/role/admin/create.blade.php
+++ resources/views/role/admin/create.blade.php
@@ -12,7 +12,7 @@
             <div class="collapse navbar-collapse w-auto h-auto" id="sidenav-collapse-main">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link  active" href="{{ route('home') }}">
+                <a class="nav-link " href="{{ route('home') }}">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                     <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                         <title>shop </title>
@@ -32,7 +32,7 @@
                 </a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link  " href="{{ asset('pages/tables.html') }}">
+                <a class="nav-link active " href="{{ url('admin/user') }}">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                     <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                         <title>office</title>
@@ -48,7 +48,7 @@
                         </g>
                     </svg>
                     </div>
-                    <span class="nav-link-text ms-1">Track</span>
+                    <span class="nav-link-text ms-1">User</span>
                 </a>
                 </li>
                 <li class="nav-item">
