58000df10c5f1d10c1a600bacbc019e5a7b3f7a7
Adam Farizi <adamfarizi4@gmail.com>
2023-09-25 21:26:40 +0700

    update

--- resources/views/auth/profile/profile.blade.php
+++ resources/views/auth/profile/profile.blade.php
@@ -173,7 +173,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="page-header min-height-300 border-radius-xl" style="background-image: url('../assets/img/curved-images/curved0.jpg'); background-position-y: 50%;">
+        <div class="page-header min-height-300 border-radius-xl" style="background-image: url('../assets/img/local/bg_login2.png'); background-position-y: 50%;">
             <span class="mask bg-gradient-primary opacity-6"></span>
         </div>
         <div class="card card-body blur shadow-blur mx-4 mt-n6 overflow-hidden">
