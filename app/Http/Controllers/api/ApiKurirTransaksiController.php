8267fdc4a265f4be09f8ed493aeb03e7a2da8f99
rayyaalika <rayyaalika@gmail.com>
2023-11-06 17:17:44 +0700

    nyah

--- app/Http/Controllers/api/ApiKurirTransaksiController.php
+++ app/Http/Controllers/api/ApiKurirTransaksiController.php
@@ -4,42 +4,49 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Transaksi;
+use App\Models\Agen;
 use Illuminate\Http\Request;
 
 class ApiKurirTransaksiController extends Controller
 {
-    public function pesanan_dikirim(string $id){
-        
-        $dikirim = Transaksi::where('status_pengiriman', 'dikirim')
-        ->whereHas('pengiriman', function ($query) use ($id) {
-            $query->where('id_kurir', $id);
-        })->get();
+    public function pesanan_dikirim(string $id)
+    {
 
-        $resi_pengiriman = [];
+        $data = Agen::select(
+            'agen.id_agen',
+            'agen.name',
+            'agen.koordinat',
+            'agen.alamat',
+            Transaksi::raw('(SELECT COUNT(*) FROM transaksi WHERE transaksi.id_agen = agen.id_agen AND transaksi.status_pengiriman = "dikirim") as jumlah_pesanan')
+        )
+            ->join('transaksi', 'transaksi.id_agen', '=', 'agen.id_agen')
+            ->join('pengiriman', 'transaksi.id_pengiriman', '=', 'pengiriman.id_pengiriman')
+            ->where('pengiriman.id_kurir', $id)
+            ->where('status_pengiriman', 'dikirim')
+            ->distinct()
+            ->get();
 
-        foreach ($dikirim as $transaksi) {
-            $resi_pengiriman = $transaksi->pengiriman->first()->resi_pengiriman;
-        }
 
-        if ($dikirim->isEmpty()) {
+
+        if ($data->isEmpty()) {
             return response()->json([
                 'success' => false,
                 'message' => 'Data tidak ditemukan',
-            ], 200); 
-        }
-        else{
+            ], 200);
+        } else {
             return response()->json([
                 'success' => true,
                 'message' => 'Data ditemukan',
-                'resi' => $resi_pengiriman,
-                'datauser' => $dikirim,
-            ], 200); 
+                'datauser' => $data,
+            ], 200);
         }
     }
 
-    public function detail_pesanan_agen(string $id){
+    public function detail_pesanan_agen(string $id)
+    {
         $data = Transaksi::where('id_agen', $id)
-        ->join('pengiriman', 'transaksi.id_pengiriman', '=', 'pengiriman.id_pengiriman')
+            ->join('pengiriman', 'transaksi.id_pengiriman', '=', 'pengiriman.id_pengiriman')
+            ->join('gas', 'transaksi.id_gas', '=', 'gas.id_gas')
             ->get();
 
 
@@ -47,28 +54,28 @@ class ApiKurirTransaksiController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Data tidak ditemukan',
-            ], 200); 
-        }
-        else{
+            ], 200);
+        } else {
             return response()->json([
                 'success' => true,
                 'message' => 'Data ditemukan',
                 'datauser' => $data,
-            ], 200); 
+            ], 200);
         }
     }
 
-    public function pesanan_selesai($id){
+    public function pesanan_selesai($id)
+    {
 
         $transaksi = Transaksi::find($id);
-    
+
         if (!$transaksi) {
             return response()->json([
                 'success' => false,
                 'message' => 'Transaksi tidak ditemukan',
             ], 404);
         }
-    
+
         // Periksa apakah transaksi sudah diterima sebelumnya
         if ($transaksi->status_pengiriman === 'Diterima') {
             return response()->json([
@@ -76,16 +83,15 @@ class ApiKurirTransaksiController extends Controller
                 'message' => 'Transaksi sudah diterima sebelumnya',
             ], 400);
         }
-    
+
         // Perbarui status pengiriman menjadi 'Diterima'
         $transaksi->status_pengiriman = 'Diterima';
         $transaksi->save();
-    
+
         return response()->json([
             'success' => true,
             'message' => 'Pesanan sudah diterima',
             'data' => $transaksi,
         ]);
     }
-    
 }
